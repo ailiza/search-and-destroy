86e0b47816d3e919a1c734ba5dd827218913e374
Annie Ilizarov <annie.ilizarov@gmail.com>
2020-10-28 17:01:36 -0400

    looper completed

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,24 +1,32 @@
 "use strict";
 
+//pseudo code
+//need to return true if target is in the array
+//our array is already ordered, so we can find a midpoint and cut it in half
+//ex. if the ordered list is 1-10 and the midpoint is 5, and our target is 3
+//so 3 < 5 --> we toss out the right half and only search the left half
+//else search the right half
+//after it searches the target that it should be in and if it's not found -->
+//need to return false if target is not in the array
+
+//case where if the target === midpoint just return true without searching left or right
+
 // Complete this algo
 const binarySearch = (array, target) => {
-  // console.log("array", array);
-  // console.log("target", target);
-  const midpoint = Math.floor(array.length / 2);
-  if (array[midpoint] === target) return true;
-  else {
-    if (target < array[midpoint]) {
-      for (let i = 0; i <= midpoint; i--) {
-        let char = array[midpoint - i];
-        if (char === target) return true;
-      }
-      for (let i = 0; i <= midpoint; i++) {
-        let char = array[midpoint + i];
-        if (char === target) return true;
-      }
-    }
-  }
-  return false;
+	//here is our midpoint which is our index
+	const midpoint = Math.floor(array.length / 2);
+	if (array.length === 1 && array[0] !== target) return false;
+	if (array[midpoint] === target) {
+		return true;
+	} else {
+		//left side
+		if (target < array[midpoint]) {
+			return binarySearch(array.slice(0, midpoint), target);
+		} else {
+			//right side
+			return binarySearch(array.slice(midpoint), target);
+		}
+	}
 };
 
 /*
--- looper/isloop.js
+++ looper/isloop.js
@@ -1,10 +1,29 @@
-'use strict';
+"use strict";
+//pseudocode - looking through the linked list to see if there is a loop
+//the start of the list is the head
+//we can set an object to put values in them
+//need an if/else statement
+//if we hit the value then return true
+//if we don't hit the value then we keep going to the next node
+//otherwise return false because we haven't found a loop
 
 //Complete this algo
 const isLoop = (linkedlist) => {
+	// console.log("linky", linkedlist);
 
+	let node = linkedlist.head;
+	let table = {};
+
+	while (node.next) {
+		if (table[node.value]) {
+			return true;
+		} else {
+			table[node.value] = node.value;
+			node = node.next;
+		}
+	}
+	return false;
 };
-
 
 /*
 EXTRA CREDIT:
@@ -14,4 +33,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
