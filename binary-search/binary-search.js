4980d8c150e00d9551199e62f5113b585cd4880a
heathernoto <heather.noto@gmail.com>
2020-10-28 16:20:02 -0400

    doh

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,8 +1,24 @@
-'use strict';
+"use strict";
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+  // console.log("array", array);
+  // console.log("target", target);
+  const midpoint = Math.floor(array.length / 2);
+  if (array[midpoint] === target) return true;
+  else {
+    if (target < array[midpoint]) {
+      for (let i = 0; i <= midpoint; i--) {
+        let char = array[midpoint - i];
+        if (char === target) return true;
+      }
+      for (let i = 0; i <= midpoint; i++) {
+        let char = array[midpoint + i];
+        if (char === target) return true;
+      }
+    }
+  }
+  return false;
 };
 
 /*
@@ -14,4 +30,4 @@ const binarySearch = (array, target) => {
 
 */
 
-module.exports = binarySearch
+module.exports = binarySearch;
